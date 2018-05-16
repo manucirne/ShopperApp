1fa8d2ea274cc26d9bfecfed9e2d2513f96b7af9
emanuellemoco <emanuellesmoco@gmail.com>
2018-05-16 12:08:14 -0300

    jumpacativity e xml sem saber se funciona

---
+++ app/src/main/java/desagil/shopper/shapp/JumpActivity.java
@@ -0,0 +1,73 @@
+package desagil.shopper.shapp;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class JumpActivity extends AppCompatActivity {
+    String justification = "";
+
+
+    //função para ir para DeliveryActivity
+    private void openDeliveryActivity() {
+        // Exemplo de código para abrir uma activity.
+        Intent intent = new Intent(this, DeliveryActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    //se mudar o nome da deliveryactivty mudar aqui **********88
+    //função para ir para DeliveryActivity NewDeliveryActivity
+    private void openNewDeliveryActivity() {
+        // Exemplo de código para abrir uma activity.
+        Intent intent = new Intent(this, NewDeliveryActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+
+        //criar botão para envair a justificativa
+        Button button_send = (Button) findViewById(R.id.button_send);
+        Button button_back = (Button) findViewById(R.id.button_back);
+
+        //para pegar a mensagem
+        final TextView text = (TextView) findViewById(R.id.textView);
+        text.setText(justification);
+
+
+        //Atividade do botão enviar - Vai para a página de próxima entrega:
+        button_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //aqui tem que fazer algo com a variavel justification - enviar para o servidos
+                openNewDeliveryActivity();  //NewDeliveryActivity - pag do wes (proxima entrega)
+
+            }
+        });
+
+        //Atividade do botão VOLTAR - Volta para a página de próxima descriçaõ:
+        button_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDeliveryActivity();
+
+            }
+        });
+
+    }
+}
+
+
