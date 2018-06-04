9aee919e90d978069eb13e0b811cc7ddadef54d6
decoejz <decoejz@gmail.com>
2018-06-04 19:18:46 -0300

    class created

---
+++ app/src/main/java/desagil/shopper/shapp/Data.java
@@ -0,0 +1,17 @@
+package desagil.shopper.shapp;
+
+public class Data {
+
+    //nome da pessoa
+    //imagem
+    //numero de caixas
+
+    public data(){}
+
+    public void setName(){}
+
+    public void setNumBoxes(int num){}
+
+    public void setPhoto(){}
+
+}
--- app/src/main/java/desagil/shopper/shapp/DataProcess.java
+++ app/src/main/java/desagil/shopper/shapp/DataProcess.java
@@ -2,4 +2,15 @@ package desagil.shopper.shapp;
 
 public class DataProcess {
 
+    public void getName(){}
+
+    public void getAddress(){}
+
+    public void getNumBoxes(){}
+
+    public void getId(){}
+
+    public void sendData(String name, ){}
+
+    public void sendJustify(){}
 }
